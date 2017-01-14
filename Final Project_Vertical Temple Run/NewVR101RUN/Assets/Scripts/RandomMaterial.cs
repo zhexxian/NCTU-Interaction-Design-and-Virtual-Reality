0ca4a4a0361c8907e9014d51647854298b36d1ea
zhexxxian <zhangzhexian@outlook.com>
2017-01-14 14:47:07 +0800

    Update final project

---
+++ Final Project_Vertical Temple Run/NewVR101RUN/Assets/Scripts/RandomMaterial.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomMaterial : MonoBehaviour {
+	// Use this for initialization
+	void Awake () {
+		GetComponent<Renderer> ().material = GetRandomMaterial ();
+    }
+	
+
+
+    /// <summary>
+    /// helper method to get a random color
+    /// </summary>
+    /// <returns></returns>
+	public Material GetRandomMaterial()
+    {
+		int x = Random.Range (0, 10);
+        if (x == 0)
+			return Resources.Load("Materials/Materials/101-section") as Material;
+        else
+			return Resources.Load("Materials/Materials/101-normal") as Material;
+    }
+
+}
---
+++ Final Project_Vertical Temple Run/NewVR101RUN/Assets/character.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class character : MonoBehaviour {
+
+	public GameObject charac;
+	public GameObject head;
+
+	Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		offset = charac.transform.position - head.transform.position + new Vector3(-0.03f,0.8f,0.1f);
+		transform.Translate (offset);
+	}
+}
